de89a50f4ba9262ac96534851c7a1c15ba92acda
Kristian S <dakraid@digital-dragons.de>
2019-04-20 00:58:40 +0200

    Major restructuring within the main code
    Made the read and write functions for the configuration more generic, created constant variables which set the filename for configuration, separated the two config files properly so their structs are no longer linked, prepared to give the ticker a big change, added function to create/download config files if not present

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"encoding/xml"
 	"flag"
 	"fmt"
-	"image/color"
 	"io"
 	"io/ioutil"
 	"os"
@@ -23,54 +22,49 @@ import (
 )
 
 // Type definition for the XML data
-type preset struct {
-	XMLName xml.Name `xml:"preset"`
+type modutil struct {
+	XMLName xml.Name `xml:"modutil"`
 	Durl    string   `xml:"durl"`
 	Lhash   string   `xml:"lhash"`
 	Qhash   string   `xml:"qhash"`
 	Mhash   string   `xml:"mhash"`
 }
 
-type user struct {
-	XMLName xml.Name `xml:"user"`
+type config struct {
+	XMLName xml.Name `xml:"config"`
 	Idir    string   `xml:"idir"`
 	Vers    uint8    `xml:"vers"`
 }
 
-type modutil struct {
-	XMLName xml.Name `xml:"modutil"`
-	Preset  preset   `xml:"preset"`
-	User    user     `xml:"user"`
-}
-
-// Specify the log file location
+// Constant variable declarations
 const logPath = "output.log"
+const confName = "config.xml"
+const moduName = "modutil.xml"
 
 // Variable declaration
 var (
 	// Flags for the logger setting up the verbose level
-	verbose                    = flag.Bool("verbose", false, "print info level logs to stdout")
+	verbose = flag.Bool("verbose", false, "print info level logs to stdout")
 
-	inDir                      = &nucular.TextEditor{}
-	resp                       = &grab.Response{}
-	conf                        = modutil{}
-	isLoaded, imsg, dmsg, prog = false, "Integrity Check", "Welcome", 0
+	lf   = &os.File{}
+	edir = &nucular.TextEditor{}
+	resp = &grab.Response{}
 
-	// Colors to be used for dcolor
-	green                      = color.RGBA{100, 255, 100, 255}
-	red                        = color.RGBA{255, 100, 100, 255}
-	white                      = color.RGBA{255, 255, 255, 255}
+	modu = modutil{}
+	conf = config{}
 
-	// Color variable used for coloration of the status ticker
-	dcolor                     = white
+	dmsg, imsg, prog = "", "Integrity Check", 0
+
 )
 
 // Error check function
 // Mainly for cleanliness
-func check(e error) {
+func check(e error) (haserr bool) {
 	if e != nil {
-		logger.Fatalf(e.Error())
+		logger.Errorf(e.Error())
+		return true
 	}
+	return false
 }
 
 ////////////////////
@@ -81,9 +75,10 @@ func check(e error) {
 // Sets up the logger, reads the config files, and sets up the UI
 // then all work is passed to the UI update function and buttons
 func main() {
+	var err error
 	flag.Parse()
 
-	lf, err := os.OpenFile(logPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0660)
+	lf, err = os.OpenFile(logPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0660)
 	if err != nil {
 		logger.Fatalf("Failed to open log file: %v", err)
 	}
@@ -93,15 +88,21 @@ func main() {
 
 	wnd := nucular.NewMasterWindow(0, "7DTD ModUtil", updatefn)
 	wnd.SetStyle(style.FromTheme(style.DarkTheme, 1.0))
-	readConfig("preset.xml")
-    // TODO: If user.xml doesn't exist, create it
-	readConfig("user.xml")
+
+	// Always recreate the modutil.xml to have the most recent version
+	createModConfig()
+	// Read the configuration files
+	readXML(moduName, modu)
+	readXML(confName, conf)
+
+	// This go function call is responsible to refresh the UI by calling wnd.changed every second
 	go func() {
 		for {
 			time.Sleep(1 * time.Second)
 			wnd.Changed()
 		}
 	}()
+
 	wnd.Main()
 }
 
@@ -114,28 +115,20 @@ func updatefn(w *nucular.Window) {
 	w.Row(40).Ratio(0.7, 0.3)
 	w.Label("7 Days to Die Mod Util", "LC")
 	if w.ButtonText("Save Config") {
-		writeConfig()
-		dmsg = "Config written"
-		dcolor = green
+		writeXML(confName)
 	}
 
 	w.Row(10).Dynamic(1)
 
 	w.Row(40).Ratio(0.4, 0.6)
 	w.Label("Please set your 7DTD directory:", "LC")
-	inDir.Edit(w)
+	edir.Edit(w)
 
 	w.Row(20).Dynamic(1)
 
 	w.Row(40).Dynamic(1)
 	if w.ButtonText("Check Integrity") {
-		if checkIntegrity() {
-			dmsg = "Integrity check passed"
-			dcolor = green
-		} else {
-			dmsg = "Integrity check failed"
-			dcolor = red
-		}
+		checkIntegrity()
 	}
 	w.Row(50).Dynamic(1)
 	w.Label(imsg, "CT")
@@ -164,35 +157,111 @@ func updatefn(w *nucular.Window) {
 	w.Progress(&prog, 100, false)
 
 	w.Row(20).Dynamic(1)
-	w.LabelColored(dmsg, "LT", dcolor)
+	w.Label(dmsg, "LT")
 
+	// If there is data being transmitted we execute the updateProgress function
 	if resp.BytesPerSecond() > 0 {
 		updateProgress()
 	}
 }
 
-//////////////////////
-// Reused functions //
-//////////////////////
+///////////////////////
+// Refresh functions //
+///////////////////////
+
+// This refreshes the values for the progress display and is called within the main UI function
+func updateProgress() {
+	if !resp.IsComplete() {
+		logger.Infof("Transferred %v / %v bytes (%.2f%%)", resp.BytesComplete(), resp.Size, 100*resp.Progress())
+		prog = int(100 * resp.Progress())
+	} else {
+		if err := resp.Err(); err != nil {
+			logger.Errorf("Download failed: %v", err)
+		} else {
+			logger.Infof("Download saved to ./%v", resp.Filename)
+		}
+	}
+}
+
+func updateTicker() {
+	// TODO: Implement Ticker
+}
+
+///////////////////
+// XML functions //
+///////////////////
 
 // Handles the download of the primary XML through HTTP
-func downloadPreset() {
-	if _, err := os.Stat("preset.xml"); err != nil {
+func createModConfig() {
+	if _, err := os.Stat(moduName); err != nil {
 		if os.IsNotExist(err) {
 			client := grab.NewClient()
-			req, _ := grab.NewRequest(".", "https://mods.netrve.net/7D2D/preset.xml")
-			logger.Infof("Downloading %v...", req.URL())
-			dmsg = fmt.Sprintf("Downloading %v...", req.URL())
-			dcolor = green
+			req, _ := grab.NewRequest(".", "https://mods.netrve.net/7D2D/"+moduName)
 			resp = client.Do(req)
+
+			logger.Infof("Downloading %v...", req.URL())
 			logger.Infof("  %v", resp.HTTPResponse.Status)
 		}
 	}
 }
 
+// Create an empty config.xml
+func createUserConfig() {
+	output, err := xml.MarshalIndent(conf, "  ", "    ")
+	check(err)
+
+	absPath, _ := filepath.Abs(confName)
+	err = ioutil.WriteFile(absPath, output, 0644)
+	check(err)
+
+	logger.Info("Created " + confName)
+}
+
+// Reads the given XML
+func readXML(filename string, target interface{}) {
+	absPath, _ := filepath.Abs(filename)
+	xmlFile, err := os.Open(absPath)
+	if check(err) {
+		switch filename {
+		case confName:
+			createUserConfig()
+		case moduName:
+			createModConfig()
+		}
+	}
+	defer xmlFile.Close()
+
+	data, err := ioutil.ReadAll(xmlFile)
+	check(err)
+
+	err = xml.Unmarshal([]byte(data), &target)
+	check(err)
+
+	edir.InsertMode = true
+	edir.Cursor = 0
+	edir.Text([]rune(conf.Idir))
+}
+
+// Writes to the given XML
+func writeXML(filename string) {
+	output, err := xml.MarshalIndent(conf, "  ", "    ")
+	check(err)
+
+	absPath, _ := filepath.Abs(filename)
+	err = ioutil.WriteFile(absPath, output, 0644)
+	check(err)
+
+	logger.Info("Finished writing " + filename)
+}
+
+/////////////////////////
+// Integrity functions //
+/////////////////////////
+
 // Generates a SHA-1 for either a given file or directory, based on the second parameter
 func genHash(filein string, isdir bool) string {
 	var result string
+
 	if !isdir {
 		f, err := os.Open(filein)
 		check(err)
@@ -205,70 +274,13 @@ func genHash(filein string, isdir bool) string {
 
 		result = hex.EncodeToString(h.Sum(nil))
 	} else {
-		hash, err := hashdir.Create(conf.User.Idir+"\\Mods", "sha1")
+		hash, err := hashdir.Create(conf.Idir+"\\Mods", "sha1")
 		check(err)
+
 		result = hash
 	}
+
 	return result
-}
-
-// This refreshes the values for the progress display and is called within the main UI function
-func updateProgress() {
-	if !resp.IsComplete() {
-		dmsg = fmt.Sprintf("Transferred %v / %v bytes (%.2f%%)", resp.BytesComplete(), resp.Size, 100*resp.Progress())
-		logger.Infof(dmsg)
-		prog = int(100 * resp.Progress())
-	} else {
-		if err := resp.Err(); err != nil {
-			logger.Errorf("Download failed: %v", err)
-			dmsg = fmt.Sprintf("Download failed: %v", err)
-			dcolor = red
-		} else {
-			dmsg = fmt.Sprintf("Download saved to ./%v", resp.Filename)
-			dcolor = green
-		}
-		logger.Infof(dmsg)
-	}
-}
-
-///////////////
-// Functions //
-///////////////
-
-// Reads the given XML config into v
-func readConfig(filename string) {
-	downloadPreset()
-	absPath, _ := filepath.Abs(filename)
-	xmlFile, err := os.Open(absPath)
-	check(err)
-	defer xmlFile.Close()
-
-	data, err := ioutil.ReadAll(xmlFile)
-	check(err)
-
-	err = xml.Unmarshal([]byte(data), &conf)
-	check(err)
-
-	inDir.InsertMode = true
-	inDir.Cursor = 0
-	inDir.Text([]rune(conf.User.Idir))
-
-	isLoaded = true
-}
-
-// Writes v into user.xml
-func writeConfig() {
-	if isLoaded {
-		output, err := xml.MarshalIndent(conf, "  ", "    ")
-		check(err)
-
-		absPath, _ := filepath.Abs("user.xml")
-		err = ioutil.WriteFile(absPath, output, 0644)
-		check(err)
-		logger.Info("Finished writing user.xml")
-	} else {
-		logger.Errorf("Please load user.xml before attempting to write")
-	}
 }
 
 // Integrity check for the main directories and files
@@ -277,15 +289,15 @@ func writeConfig() {
 func checkIntegrity() bool {
 	var hash1, hash2, hash3 string
 
-	hash1 = genHash(conf.User.Idir+"\\Data\\Config\\Localization.txt", false)
-	hash2 = genHash(conf.User.Idir+"\\Data\\Config\\Localization - Quest.txt", false)
-	hash3 = genHash(conf.User.Idir+"\\Mods", true)
+	hash1 = genHash(conf.Idir+"\\Data\\Config\\Localization.txt", false)
+	hash2 = genHash(conf.Idir+"\\Data\\Config\\Localization - Quest.txt", false)
+	hash3 = genHash(conf.Idir+"\\Mods", true)
 
 	logger.Infof("Hash 1: %s | Hash 2: %s | Hash 3: %s", hash1, hash2, hash3)
 
-	pass1 := strings.EqualFold(hash1, conf.Preset.Lhash)
-	pass2 := strings.EqualFold(hash2, conf.Preset.Qhash)
-	pass3 := strings.EqualFold(hash3, conf.Preset.Mhash)
+	pass1 := strings.EqualFold(hash1, modu.Lhash)
+	pass2 := strings.EqualFold(hash2, modu.Qhash)
+	pass3 := strings.EqualFold(hash3, modu.Mhash)
 
 	logger.Infof("Pass 1: %t | Pass 2: %t | Pass 3: %t", pass1, pass2, pass3)
 
@@ -298,41 +310,47 @@ func checkIntegrity() bool {
 	return false
 }
 
+////////////////////////
+// Download Functions //
+////////////////////////
+
 // Handles the main download for the BASE pack on which everything else is applied on top of
 func downloadBase() {
-	if conf.User.Vers < 1 {
+	if conf.Vers < 1 {
 		if _, err := os.Stat("7DTD_BASE.7z"); err != nil {
 			if os.IsNotExist(err) {
 				client := grab.NewClient()
-				req, _ := grab.NewRequest(".", conf.Preset.Durl+"7DTD_BASE.7z")
+				req, _ := grab.NewRequest(".", modu.Durl+"7DTD_BASE.7z")
 				logger.Infof("Downloading %v...", req.URL())
-				dmsg = fmt.Sprintf("Downloading %v...", req.URL())
-				dcolor = green
 				resp = client.Do(req)
+
 				logger.Infof("  %v", resp.HTTPResponse.Status)
 			}
 		} else {
 			prog = 100
-			dmsg = "7DTD_BASE.7z already downloaded"
-			dcolor = green
+			logger.Info("File 7DTD_BASE already exists")
 		}
 	}
 }
 
 func downloadUpdate() {
-	if conf.User.Vers > 0 {
+	if conf.Vers > 0 {
 		// TODO: Implement Update Download
 	}
 }
 
+///////////////////////
+// Install Functions //
+///////////////////////
+
 func installBase() {
-	if conf.User.Vers < 1 {
+	if conf.Vers < 1 {
 		// TODO: Implement Base Install
 	}
 }
 
 func installUpdate() {
-	if conf.User.Vers > 0 {
+	if conf.Vers > 0 {
 		// TODO: Implement Update Install
 	}
 }
